ab4dffe1415ff89bfd5f049d90e49f978a5a563a
Jason Li <jasonlilb2@gmail.com>
2026-04-13 22:11:57 +1000

    parser: declare basic ast nodes

---
+++ src/frontend/ast.rs
@@ -0,0 +1,30 @@
+
+
+#[derive(Debug)]
+pub enum Stmt {
+    Print(Expr),
+    ExprStmt(Expr),
+}
+
+#[derive(Debug)]
+pub enum Expr {
+    IntegerLiteral(i64),
+    Binary{
+        left: Box<Expr>,
+        op: BinaryOp,
+        right: Box<Expr>
+    }
+}
+
+#[derive(Debug)]
+pub enum BinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div
+}
+
+#[derive(Debug)]
+pub struct Program {
+    pub statements: Vec<Stmt>
+}
